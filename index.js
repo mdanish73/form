cfe699be8d65255ce0f3e6665d29eff5756a6312
mdanish73 <mdiqbal00037@gmail.com>
2023-09-16 17:49:02 +0500

    rendering ejs api updated

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ app.use("/form/registration", studentRoutes);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/form", function(req, res) {
-  res.render('form');
+app.get("form", function(req, res) {
+  res.render(__form+'/views/form.ejs');
 });
+
+// mongoose.connect("mongodb://127.0.0.1:27017").then((res) => {
+//     console.log("Database Connection established");
+// }).catch((error) => {
+//     console.log(error.message);
+// });
 
 mongoose.connect("mongodb+srv://mdiqbal00037:Danish-73@students.ir24p0i.mongodb.net/students?retryWrites=true&w=majority").then((res) => {
     console.log("Database Connection established");
